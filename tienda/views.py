4a118f09c463feab7cfb9f8f8108a2d9981d5121
UnaiSuarez <unai64535@gmail.com>
2021-12-10 02:11:51 +0100

    Agregar amigos

--- tienda/views.py
+++ tienda/views.py
@@ -39,9 +39,10 @@ class VideojuegosListView(generic.ListView):
             videojuegos = Videojuego.objects.filter(title__icontains=busqueda)
         else:
             pk = self.request.GET.get('pk')
+            print("pk")
             print(pk)
             if pk:
-                videojuegos = Videojuego.objects.filter(genre__name="shooter")
+                videojuegos = Videojuego.objects.filter(genre__name=pk)
             else:
                 videojuegos = Videojuego.objects.all()
                 
@@ -96,30 +97,30 @@ class AmigosUsuarioListView(LoginRequiredMixin, generic.ListView):
     template_name = 'amigos.html'
 
 # en este momento no funcional, no lo borro ya que es otra posibilidad
-class AñadirSaldo(LoginRequiredMixin, SuccessMessageMixin, generic.UpdateView):
-    model = User
-    fields = ['saldo']
-    template_name = 'añadirSaldo.html'
-    success_url = '/'
-    success_message = " se ha añadido el saldo correctamente"
-    def get_initial(self):
-        return {'saldo': ''}
+# class AñadirSaldo(LoginRequiredMixin, SuccessMessageMixin, generic.UpdateView):
+#     model = User
+#     fields = ['saldo']
+#     template_name = 'añadirSaldo.html'
+#     success_url = '/'
+#     success_message = " se ha añadido el saldo correctamente"
+#     def get_initial(self):
+#         return {'saldo': ''}
     
-    def get_object(self):
-        usuario  = super().get_object()
-        self.saldoanterior = usuario.saldo
-        return usuario
+#     def get_object(self):
+#         usuario  = super().get_object()
+#         self.saldoanterior = usuario.saldo
+#         return usuario
 
-    def form_valid(self, form):
-        saldoanterior = self.saldoanterior
-        saldoanterior = saldoanterior
-        nuevo = int(form.cleaned_data['saldo'])
-        form.instance.saldo = saldoanterior + nuevo
-        return super().form_valid(form)
+#     def form_valid(self, form):
+#         saldoanterior = self.saldoanterior
+#         saldoanterior = saldoanterior
+#         nuevo = int(form.cleaned_data['saldo'])
+#         form.instance.saldo = saldoanterior + nuevo
+#         return super().form_valid(form)
     
-    def get_context_data(self, **kwargs):
-        context = super(AñadirSaldo, self).get_context_data(**kwargs)
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super(AñadirSaldo, self).get_context_data(**kwargs)
+#         return context
 
 def crear_usuario(request):
     if request.method == 'POST':
@@ -213,30 +214,46 @@ def contact(request):
     datos.update({'form': ContactForm()})
     return render(request,'contact.html', context=datos)
 
+
+class UsuariosListView(generic.ListView):
+    '''
+    Vista genérica para nuestro listado de autores
+    '''
+    model = User
+    paginate_by = 10
+    template_name = 'usuarios.html'
+    
+    def get_queryset(self):
+        busqueda = self.request.GET.get('q')
+        if busqueda:
+            usuarios = User.objects.filter(username__icontains=busqueda)
+        else:
+            usuarios = User.objects.all()
+        return usuarios
+
+
 def AñadirAmigo(request, pk):
     if request.method == 'GET':
-        videojuego = Videojuego.objects.filter(title = pk)
-        datos.update({'videojuego_list': videojuego, 'pk':pk})
+        amigoNuevo = User.objects.filter(username = pk)
+        datos.update({'usuario': amigoNuevo, 'pk':pk})
     elif request.method == 'POST':
         usuario = request.user
         if usuario.is_anonymous:
             messages.add_message(request, messages.SUCCESS,'Usted no ha iniciado sesion')
             return redirect('/accounts/login')
         else:
-            videojuegos = Videojuego.objects.filter(title = pk)
-            for videojuego in videojuegos:
-                if videojuego in usuario.juegosComprados.all():
-                   messages.add_message(request, messages.SUCCESS,'Usted ya tiene comprado este videojuego.')
+            amigoNuevo = User.objects.filter(username = pk)
+            for amigoN in amigoNuevo:
+                if amigoN in usuario.amigos.all():
+                   messages.add_message(request, messages.SUCCESS,'Usted ya tiene agregada a esta persona.')
                 else:
-                    if usuario.saldo >= videojuego.precio:
-                        usuario.saldo = usuario.saldo - videojuego.precio
-                        usuario.juegosComprados.add(videojuego)
-                        messages.add_message(request, messages.SUCCESS,'Juego comprado.')
-                        usuario.save()
-                    else:
-                     messages.add_message(request, messages.SUCCESS,'No tienes dinero suficiente.')
+                    amigoN.amigos.add(usuario)
+                    usuario.amigos.add(amigoN)
+                    messages.add_message(request, messages.SUCCESS,'Amigo añadido.')
+                    usuario.save()
+                    amigoN.save()
         
             
         return redirect('/')
     
-    return render(request, 'videojuego.html', context=datos)
+    return render(request, 'agregarAmigo.html', context=datos)
--- tiendaVideojuegos/urls.py
+++ tiendaVideojuegos/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls.conf import include
 
-from tienda.views import AmigosUsuarioListView, EliminarVideojuego, ModificarVideojuego, VideojuegoListView, VideojuegosListView, VideojuegosUsuarioListView, AñadirSaldo, AñadirSaldo2, contact, crear_tarjeta, crear_usuario
+from tienda.views import AmigosUsuarioListView, EliminarVideojuego, ModificarVideojuego, UsuariosListView, VideojuegoListView, VideojuegosListView, VideojuegosUsuarioListView, AñadirAmigo, AñadirSaldo2, contact, crear_tarjeta, crear_usuario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,6 +36,8 @@ urlpatterns = [
     path('contacto/', contact, name ='contacto'),
     path('editarVideojuego/<str:pk>', ModificarVideojuego.as_view(), name='editarVideojuego'),
     path('eliminarVideojuego/<str:pk>', EliminarVideojuego.as_view(), name='eliminarVideojuego'),
+    path('usuarios/', UsuariosListView.as_view(), name='usuarios'),
+    path('usuario/<str:pk>', AñadirAmigo, name='usuario')
 ]
 
 
